a184322212028ebd353cc0169646ad0eb164210b
FrotaSaude <carloseduardobergamo87@gmail.com>
2026-04-24 15:06:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,25 @@
-const CACHE_NAME = 'digifrota-v3.2'; // ALTERE SEMPRE QUE ATUALIZAR
+const CACHE_NAME = 'digifrota-v3.2';
 
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-// install
-self.addEventListener('install', event => {
+self.addEventListener('install', e => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
 });
 
-// activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener('activate', e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// fetch (network first)
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
+      .then(res => {
+        const clone = res.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return response;
+        return res;
       })
       .catch(() => caches.match(event.request))
   );
