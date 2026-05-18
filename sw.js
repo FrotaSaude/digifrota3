73ef68473239112422a193ca1bd8f67f0cca37ee
FrotaSaude <carloseduardobergamo87@gmail.com>
2026-05-17 22:39:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'digifrota-v3-2-20260514c'; // ← versão atualizada
+const CACHE_NAME = 'digifrota-v3-2-20260517a'; // ← versão atualizada
 const ASSETS = [
   './',
   './index.html',
